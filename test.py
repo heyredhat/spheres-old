ad0babd21860893d517e3e57f0a08cdf8e11d071
Matthew Weiss <heyredhat@chip.fios-router.home>
2020-02-21 04:23:06 -0500

    oyu

--- test.py
+++ test.py
@@ -15,16 +15,16 @@ s = View(np.array([1,1]),\
 		 	XYZ_spin(data["stars"]),\
 		 js_class="Sphere")
 
-u = (-1j*0.01*qt.rand_herm(2)).expm().full()
-q = qt.rand_ket(2).full().T[0]
-input()
-s.set(q)
-a.set(q)
-for i in range(1000):
-	q = np.dot(u, q)
-	s.set(q)
-	a.set(q)
+#u = (-1j*0.01*qt.rand_herm(2)).expm().full()
+#q = qt.rand_ket(2).full().T[0]
+#input()
+#s.set(q)
+#a.set(q)
+#for i in range(1000):
+#	q = np.dot(u, q)
+#	s.set(q)
+#	a.set(q)
 	# gotta cut down on flushes and wait too
-	sockets.sleep(0.01)
+#	sockets.sleep(0.01)
 	# should be able to add random listeners
 	# on updates...
